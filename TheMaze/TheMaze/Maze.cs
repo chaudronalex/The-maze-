9d0a668a7712f668c0548d42a8aef578c4f66782
alex <alexmag96@gmail.com>
2018-11-23 09:55:18 +0100

    complexity

--- TheMaze/TheMaze/Maze.cs
+++ TheMaze/TheMaze/Maze.cs
@@ -31,25 +31,10 @@ namespace TheMaze
                     maze.Add(node);
                 }
             }
-            // choosing a random cell to begin with that is not on the edge of the maze
-
-            //int number = rand.Next(0, size * size);
-            //while (maze[number].X == 0 || maze[number].X == size || maze[number].Y == 0 || maze[number].Y == size)
-            //    number = rand.Next(0, size * size);
-
-            //Cell current = maze[number];
-            //current.Previous = new Cell(-1, -1); // for the stop condition in the Move function
-
-            //Move(current, rand.Next(0, 3), null); // choosing a random neightbor to the cell to move to
-            
             // shuffling the list to select a random cell to begin with
             var shuffled = maze.OrderBy(a => rand.Next());
 
-            // moving in the maze until every cell is visited
-            //foreach (Cell c in shuffled)
-            //if (c.Visited == false)
-            //Move(c);
-
+            // choosing a random cell to begin with that is not on the edge of the maze
             int number = rand.Next(0, size * size);
             while (maze[number].X == 0 || maze[number].X == size || maze[number].Y == 0 || maze[number].Y == size)
                 number = rand.Next(0, size * size);
@@ -80,11 +65,6 @@ namespace TheMaze
                 
             // shuffling the list of neighbours to select a random one
             var shuffled = neighbour.OrderBy(a => rand.Next());
-            //foreach(Cell c in shuffled)
-            //{
-            //    Console.Write("(" + c.X + "," + c.Y + ") ");
-            //}
-            //Console.WriteLine("");
 
             // Deep first search
             foreach (Cell c in shuffled)
@@ -98,7 +78,7 @@ namespace TheMaze
             }
         }
 
-        public Cell Search(int x, int y)
+        public Cell Search(int x, int y) // to get a cell using its coordinates
         {
             foreach (Cell c in maze)
                 if (c.X == x && c.Y == y)
@@ -107,70 +87,7 @@ namespace TheMaze
             return null;
         }
 
-        //public Cell Move2(Cell currentCell, int pick, List<Cell> neightbors)
-        //{
-        //    Random rand = new Random();
-
-        //    // creation of a temporary list with the neightbors of the cell
-        //    neightbors = new List<Cell>();
-
-            //if (Search(currentCell.X, currentCell.Y - 1) != null)
-            //    neightbors.Add(Search(currentCell.X, currentCell.Y - 1));
-
-            //if (Search(currentCell.X, currentCell.Y + 1) != null)
-            //    neightbors.Add(Search(currentCell.X, currentCell.Y + 1));
-
-            //if (Search(currentCell.X + 1, currentCell.Y) != null)
-            //    neightbors.Add(Search(currentCell.X + 1, currentCell.Y));
-
-            //if (Search(currentCell.X - 1, currentCell.Y) != null)
-                //neightbors.Add(Search(currentCell.X - 1, currentCell.Y));
-
-
-        //    // verifying that all neighbors hasn't been visited yet
-        //    bool visit = true;
-
-        //    foreach (Cell c in neightbors)
-        //    {
-        //        if (c.Visited == false)
-        //        {
-        //            visit = false;
-        //        }
-        //    }
-
-        //    // if so, going back to the previous cell
-        //    if (visit == true)
-        //    {
-        //        return Move2(currentCell.Previous, rand.Next(0, 4), null);
-        //    }
-            
-        //    else
-        //    {
-        //        // moving to the selected neightbor, creating a path between the two cells 
-        //        // only if the cell wasn't visited yet
-        //        if (neightbors[pick].Visited == false)
-        //        {
-        //            if (pick > neightbors.Count)
-        //                pick = rand.Next(0, 3);
-
-        //            if (pick > neightbors.Count)
-        //                pick = rand.Next(0, 2);
-
-        //            currentCell.Neightbors.Add(neightbors[pick]);
-        //            currentCell.Visited = true;
-        //            neightbors[pick].Previous = currentCell;
-        //            return Move2(neightbors[pick], rand.Next(0, 4), null); // repeating with the new cell
-        //        }
-
-        //        else
-        //        {
-        //            neightbors.RemoveAt(pick);
-        //            return Move2(currentCell, rand.Next(0, neightbors.Count - 1), neightbors); // choosing an another unvisited neightbor
-        //        }
-        //    }
-        //}
-
-        public bool AreNeighbours(Cell c1, Cell c2) 
+        public bool AreNeighbours(Cell c1, Cell c2) // verifying if two cells are neighbours in the graph (= no wall between them)
         {
             foreach (Cell c in c1.Neighbour)
                 if (c == c2)
@@ -181,8 +98,10 @@ namespace TheMaze
 
         public int [,] GraphToMatrix() // for the print function
         {
+            // we create an element in the matrix for each edge in the graph, so the matrix is two times bigger than the graph
             int[,] maze2 = new int[size * 2, size * 2];
 
+            // each cell is empty
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
@@ -192,6 +111,7 @@ namespace TheMaze
                     maze2[x, y] = 0;
                 }
             }
+            // putting a wall between two cells if they aren't neighbours
             for (int i = 0; i < size - 1; i++)
             {
                 for (int j = 0; j < size - 1; j++)
@@ -205,6 +125,7 @@ namespace TheMaze
                         maze2[x, y] = 1;
                 }
             }
+            // putting a wall between two cells if they aren't neighbours
             for (int i = 0; i < size - 1; i++)
             {
                 for (int j = 0; j < size - 1; j++)
@@ -218,6 +139,7 @@ namespace TheMaze
                         maze2[x, y] = 1;
                 }
             }
+            // every element that isn't a cell nor an edge is a wall
             for (int i = 0; i < size - 1; i++)
             {
                 for (int j = 0; j < size - 1; j++)
@@ -233,6 +155,8 @@ namespace TheMaze
 
         public void Print()
         {
+            int[,] matrix = GraphToMatrix();
+
             for (int a = 0; a < (size * 2) + 1; a++)
                 Console.Write("##");
             Console.Write("\n");
@@ -242,7 +166,7 @@ namespace TheMaze
                 Console.Write("##");
                 for (int j = 0; j < (size * 2) - 1; j++)
                 {
-                    if (GraphToMatrix()[i, j] == 0)
+                    if (matrix[i, j] == 0)
                         Console.Write("  ");
 
                     else
